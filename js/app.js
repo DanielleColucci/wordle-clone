15b053f8aaa4c1903b63f920071d4c84c8727048
DanielleColucci <ddcolucci99@gmail.com>
2022-12-15 21:48:40 -0500

    declare state variables and constants

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /*------------------------------- Constants -------------------------------*/
-
+import { getWord, checkWord } from "../data/words.js";
 
 /*------------------------------- Variables -------------------------------*/
-
+let difficulty, secretWord, winner, lastGuess, currentRow, currentLetter
 
 /*----------------------- Cached Element Referenes ------------------------*/
 
