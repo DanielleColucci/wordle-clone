f83945f3a0c4c412c5a6fe52d2435e2d321f6b1d
DanielleColucci <ddcolucci99@gmail.com>
2022-12-21 17:34:01 -0500

    add flip audio file and playFlip function

--- js/audio.js
+++ js/audio.js
@@ -1,10 +1,17 @@
 let click = new Audio('../assets/click.wav')
+let flip = new Audio('../assets/flip.wav')
 
 function playClick() {
   click.volume = 0.3
   click.play()
 }
 
+function playFlip() {
+  flip.volume = 0.4
+  flip.play()
+}
+
 export {
-  playClick
+  playClick,
+  playFlip
 }
